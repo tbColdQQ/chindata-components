e833bc0e9e30aaa59fe7590847e147685c3ae304
jie.niu <jie.niu@chindatagroup.com>
2020-12-09 16:51:03 +0800

    modify

---
+++ packages/table/index.js
@@ -0,0 +1,3 @@
+import ChindataTable from './src/table'
+
+export default ChindataTable
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render } from 'react-dom'
+import ChindataTable from '../packages/table/index'
+const App = () => (
+  <ChindataTable />
+)
+
+render(<App />, document.getElementById('root'))
